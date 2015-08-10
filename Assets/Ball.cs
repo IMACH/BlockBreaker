b2fd9fc7fca9c302aa88e8da410a21d66ff1cd1e
IMACH <mr.kevinmach@gmail.com>
2015-08-10 14:38:44 +0800

    Prefab Bricks , Brick Script
    1Hit, 2Hit, 3Hits
    Brick counter with maxHits integrated.

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -15,11 +15,14 @@ public class Ball : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!hasStarted) {
+			//Lock ball relative to paddle 
 			this.transform.position = paddle.transform.position + paddleToBallVector; 
+
+			//Wait for a mouse press to launch the ball.
 			if (Input.GetMouseButtonDown (0)) {
-					print ("Left mouse button clicked, launch ball"); 
+					print ("Left mouse button clicked, launch ball");
 					hasStarted = true; 
-					this.rigidbody2D.velocity = new Vector2(2f,10f); 
+					this.rigidbody2D.velocity = new Vector2(2f,5f); 
 			}
 		}
 		}
